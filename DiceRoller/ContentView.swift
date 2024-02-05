dde335473c2ee8c2b95e6f63fe920f4e7f652e6c
FALLENEZER <igumnov.danechka@mail.ru>
2024-02-05 13:02:55 +0300

    Very final

--- DiceRoller/ContentView.swift
+++ DiceRoller/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var numberOfDice: Int = 1
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dice Roller")
+                .font(.largeTitle.lowercaseSmallCaps())
+                .bold()
+            
+            HStack {
+                ForEach(1...numberOfDice, id: \.self) { _ in DiceView()
+                }
+            }
+            HStack {
+                Button("Remove", systemImage: "minus.circle.fill") {
+                    numberOfDice -= 1
+                }
+                .disabled(numberOfDice == 1)
+                
+                Button("Add", systemImage: "plus.circle.fill") {
+                    numberOfDice += 1
+                }
+                .disabled(numberOfDice == 5)
+            }
+            .padding()
+            .labelStyle(.iconOnly)
+            .font(.title)
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.backColour)
+        .tint(.black)
     }
 }
 
