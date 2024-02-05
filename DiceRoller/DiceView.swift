8a61d526c0b47193396ddebead420903d7ce8122
FALLENEZER <igumnov.danechka@mail.ru>
2024-02-05 13:02:40 +0300

    Final version

---
+++ DiceRoller/DiceView.swift
@@ -0,0 +1,18 @@
+//
+//  DiceView.swift
+//  DiceRoller
+//
+//  Created by Даниил Игумнов on 05.02.2024.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiceView()
+}
