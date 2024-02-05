50394cf38324f412ed12cbbbcfa0381210e03460
FALLENEZER <igumnov.danechka@mail.ru>
2024-02-05 09:36:21 +0300

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Даниил Игумнов on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
